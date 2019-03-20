d436016e0c1d7a9a6495a1971200288fcc7ecdd2
Can <kralciplak9@gmail.com>
2019-03-20 19:35:00 +0300

    Added new View

---
+++ Genome Calculator/AddVCCell.swift
@@ -0,0 +1,21 @@
+//
+//  AddVCCell.swift
+//  Genome Calculator
+//
+//  Created by Can Balkaya on 3/20/19.
+//  Copyright © 2019 Can Balkaya. All rights reserved.
+//
+
+import UIKit
+
+class AddVCCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
