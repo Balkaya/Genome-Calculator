d906d4283908e832142ec1f9ae38d69b7eedb3bd
Can <kralciplak9@gmail.com>
2019-03-20 19:37:34 +0300

    Added a new Cell file

---
+++ Genome Calculator/TemporaryAddVC.swift
@@ -0,0 +1,16 @@
+//
+//  TemporaryAddVC.swift
+//  Genome Calculator
+//
+//  Created by Can Balkaya on 3/20/19.
+//  Copyright © 2019 Can Balkaya. All rights reserved.
+//
+
+import UIKit
+
+class TemporaryAddVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
