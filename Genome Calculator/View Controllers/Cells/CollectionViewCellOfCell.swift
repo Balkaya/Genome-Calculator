787da107d24d1b6d02ab653bc9e2c1b8e8f15647
Can <kralciplak9@gmail.com>
2019-05-17 18:15:57 +0300

    Developed new AddVC

---
+++ Genome Calculator/View Controllers/Cells/CollectionViewCellOfCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellOfCell.swift
+//  Genome Calculator
+//
+//  Created by Can Balkaya on 5/17/19.
+//  Copyright © 2019 Can Balkaya. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellOfCell: UICollectionViewCell {
+    
+}
---
+++ Genome Calculator/View Controllers/NewAddVC.swift
@@ -0,0 +1,15 @@
+//
+//  NewAddVC.swift
+//  Genome Calculator
+//
+//  Created by Can Balkaya on 5/17/19.
+//  Copyright © 2019 Can Balkaya. All rights reserved.
+//
+
+import UIKit
+
+class NewAddVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
